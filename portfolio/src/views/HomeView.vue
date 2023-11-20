7ec885cd931790d19e74f957aff6cfd3b7d15412
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-11-20 20:07:19 +0100

    nav bar

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <img src="">
+    <h1>Alex Doerfer</h1>
+    <p>Full-Stack Software Engineer</p>
   </main>
 </template>
