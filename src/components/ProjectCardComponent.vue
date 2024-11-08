476da08f0a68f019f4092997f9b5fa98eb703643
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2024-11-08 16:01:39 +0100

    test

--- src/components/ProjectCardComponent.vue
+++ src/components/ProjectCardComponent.vue
@@ -2,7 +2,7 @@
 <template>
         <div class="pb-8">
             <div class="flex">
-                <img class="rounded" :src="(`src/assets/me.jpg`)">
+                <img class="rounded" :src="(`/assets/me.jpg`)">
                 <!-- <img class="rounded" :src="(`@/assets/${project.img}`)"> -->
             </div>
         </div>
