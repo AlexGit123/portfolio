7d31804b7cd4a82f64c903524feec8787aabd056
Alex Doerfer <alexdoerfer@mac.lan>
2025-02-21 16:23:43 +0100

    fixed eager loading for images

--- src/components/ProjectCardComponent.vue
+++ src/components/ProjectCardComponent.vue
@@ -1,33 +1,37 @@
 <template>
-    <div class="flex justify-center">
-        <img rel="preload" class="border border-indigo-400 rounded-lg max-h-auto" :src="getImageURL()">
-    </div>
+  <div class="flex justify-center">
+    <img
+      loading="eager"
+      class="border border-indigo-400 rounded-lg max-h-auto"
+      :src="getImageURL()"
+    />
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        project: {
-            type: Object,
-            required: true,
-        },
-    },
-    methods: {
-        getImageURL() {
-            try {
-                return (`/${this.project.img}`);
-            } catch (e) {
-                console.log("image not found")
-            }
-        },
+  props: {
+    project: {
+      type: Object,
+      required: true
     }
-};
+  },
+  methods: {
+    getImageURL() {
+      try {
+        return `/${this.project.img}`
+      } catch (e) {
+        console.log('image not found')
+      }
+    }
+  }
+}
 </script>
 <style scoped>
 .text-color {
-    color: #7fbdec;
+  color: #7fbdec;
 }
 
 .square-color1 {
-    background-color: #DADDD8;
+  background-color: #daddd8;
 }
 </style>
